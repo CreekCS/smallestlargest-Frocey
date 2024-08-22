5e30362fcf375f8d8dcc2ab7576e19b837e820da
Frocey <158637547+Frocey@users.noreply.github.com>
2024-08-22 19:14:18 +0000

    added the method

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,31 @@
 //put your name here
+import java.util.Arrays;
 public class SmallestLargest{
     public static void main(String[] args){
-
+        int [] nums = new int [10];
+        for (int i = 0; i < nums.length; i ++ ){
+            nums[i]= (int)(Math.random()*100);
+        }
+        System.out.println(Arrays.toString(nums));
+        System.out.println("Smallest: " + smallestValue(nums));
+        System.out.println("Largest: " +largestValue(nums));
     }
-    public static int smallestValue(){
-
+    public static int smallestValue(int [] nums){
+        int smallest = nums[0];
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]<smallest){
+                smallest = nums[i];
+            }
+        }
+        return smallest;
     }
-    public static int largestValue(){
-        
+    public static int largestValue(int [] nums){
+        int largest = nums[0];
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]>largest){
+                largest = nums[i];
+            }
+        }
+        return largest;
     }
 }
